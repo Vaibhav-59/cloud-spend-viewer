fdc6a053fcb21441b206d85aa86ba6b78113310a
Vaibhav Vataliya <vaibhavvataliya162@gmail.com>
2025-12-02 17:42:41 +0530

    vite config js add backend deploy url

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://cloud-spend-viewer.onrender.com/',
         changeOrigin: true,
       },
     },
